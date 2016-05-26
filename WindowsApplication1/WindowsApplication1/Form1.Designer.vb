d47bb0b2e174f77457eb2d4075fe319ae5db42cd
xieguigang <xie.guigang@gmail.com>
2016-05-27 00:54:50 +0800

    add player art design from qqmusic

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(541, 507)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 507)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(541, 115)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(541, 507)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(22, 115)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGreen
+        Me.ClientSize = New System.Drawing.Size(696, 622)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.LightGreen
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Call New Thread(AddressOf sousuo).Start()
+    End Sub
+
+    Private Sub sousuo()
+        run1 = True
+        run2 = False
+        Call Me.Invoke(Sub() Call __sss())
+    End Sub
+
+    Dim d As Integer = 50
+
+    ''' <summary>
+    ''' 收缩
+    ''' </summary>
+    Private Sub __sss()
+        Dim loci As Point
+
+        Do While True
+            If run2 Then
+                Exit Do
+            End If
+
+            If PictureBox1.Height > 0 Then
+                PictureBox1.Height -= d
+                loci = PictureBox1.Location
+                PictureBox1.Location = New Point(loci.X, loci.Y + d)
+            Else
+                PictureBox1.Height = 0
+                Exit Do
+            End If
+
+
+
+            Application.DoEvents()
+            Thread.Sleep(10)
+        Loop
+    End Sub
+
+    Dim run1, run2 As Boolean
+
+    ''' <summary>
+    ''' 伸展
+    ''' </summary>
+    Sub ____ss2()
+        Dim loci As Point
+
+        Do While True
+            If run1 Then
+                Exit Do
+            End If
+
+            If PictureBox1.Height < 507 Then
+                PictureBox1.Height += d
+                loci = PictureBox1.Location
+                PictureBox1.Location = New Point(loci.X, loci.Y - d)
+            Else
+                PictureBox1.Height = 507
+                Exit Do
+            End If
+
+
+
+            Application.DoEvents()
+            Thread.Sleep(10)
+        Loop
+    End Sub
+
+    Public Sub s2()
+        run1 = False
+        run2 = True
+        Call Me.Invoke(Sub() Call Me.____ss2())
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Call New Thread(AddressOf s2).Start()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Location = New Point(0, My.Computer.Screen.WorkingArea.Height - 10 - Height)
+    End Sub
+End Class
