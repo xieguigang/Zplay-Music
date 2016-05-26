70ca1647b9194aa98f39d0eaca0ef3ca31190d8d
xieguigang <xie.guigang@gmail.com>
2016-05-27 05:01:11 +0800

    departs the playlist control animation from the main form

--- Zplay-Music/Zplay-Music/FormZplay.vb
+++ Zplay-Music/Zplay-Music/FormZplay.vb
@@ -2,90 +2,10 @@
 
 Public Class FormZplay
 
-    Private Sub sousuo()
-        run1 = True
-        run2 = False
-        Call Me.Invoke(Sub() Call __sss())
-    End Sub
-
-    Dim d As Integer = 80
-
-    ''' <summary>
-    ''' 收缩
-    ''' </summary>
-    Private Sub __sss()
-        Dim loci As Point
-
-        Do While True
-            If run2 Then
-                Exit Do
-            End If
-
-            If Panel1.Height > 0 Then
-                Panel1.Height -= d
-                loci = Panel1.Location
-                Panel1.Location = New Point(loci.X, loci.Y + d)
-            Else
-                Panel1.Height = 0
-                Exit Do
-            End If
-
-
-
-            Application.DoEvents()
-            Thread.Sleep(10)
-        Loop
-    End Sub
-
-    Dim run1, run2 As Boolean
-
-    ''' <summary>
-    ''' 伸展
-    ''' </summary>
-    Sub ____ss2()
-        Dim loci As Point
-
-        Do While True
-            If run1 Then
-                Exit Do
-            End If
-
-            If Panel1.Height < 507 Then
-                Panel1.Height += d
-                loci = Panel1.Location
-                Panel1.Location = New Point(loci.X, loci.Y - d)
-            Else
-                Panel1.Height = 507
-                Exit Do
-            End If
-
-
-
-            Application.DoEvents()
-            Thread.Sleep(10)
-        Loop
-    End Sub
-
-    Public Sub s2()
-        run1 = False
-        run2 = True
-        Call Me.Invoke(Sub() Call Me.____ss2())
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
-        Call New Thread(AddressOf sousuo).Start()
-
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        Call New Thread(AddressOf s2).Start()
-    End Sub
+    Dim __playListInvoke As PlayListAnimation
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Location = New Point(0, My.Computer.Screen.WorkingArea.Height - 10 - Height)
+        __playListInvoke = New PlayListAnimation(Me)
     End Sub
 End Class
---
+++ Zplay-Music/Zplay-Music/PlayListAnimation.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Threading
+
+Public Class PlayListAnimation
+
+    Public ReadOnly Property Parent As FormZplay
+
+    Sub New(main As FormZplay)
+        Parent = main
+    End Sub
+
+    Private Sub __startCloseList()
+        run1Close = True
+        runOpen = False
+        Call Parent.Invoke(Sub() Call __closing())
+    End Sub
+
+    Dim delta As Integer = 80
+
+    ''' <summary>
+    ''' 收缩
+    ''' </summary>
+    Private Sub __closing()
+        Dim loci As Point
+
+        Do While True
+            If runOpen Then
+                Exit Do
+            End If
+
+            If Parent.Panel1.Height > 0 Then
+                Parent.Panel1.Height -= delta
+                loci = Parent.Panel1.Location
+                Parent.Panel1.Location = New Point(loci.X, loci.Y + delta)
+            Else
+                Parent.Panel1.Height = 0
+                Exit Do
+            End If
+
+            Call Thread.Sleep(10)
+            Call Application.DoEvents()
+        Loop
+    End Sub
+
+    Dim run1Close, runOpen As Boolean
+
+    ''' <summary>
+    ''' 伸展
+    ''' </summary>
+    Sub __runOpen()
+        Dim loci As Point
+
+        Do While True
+            If run1Close Then
+                Exit Do
+            End If
+
+            If Parent.Panel1.Height < 507 Then
+                Parent.Panel1.Height += delta
+                loci = Parent.Panel1.Location
+                Parent.Panel1.Location = New Point(loci.X, loci.Y - delta)
+            Else
+                Parent.Panel1.Height = 507
+                Exit Do
+            End If
+
+            Call Thread.Sleep(10)
+            Call Application.DoEvents()
+        Loop
+    End Sub
+
+    Public ReadOnly Property IsOpen As Boolean
+        Get
+            Return Parent.Panel1.Height > 0
+        End Get
+    End Property
+
+    Private Sub __startOpenList()
+        run1Close = False
+        runOpen = True
+        Call Parent.Invoke(Sub() Call Me.__runOpen())
+    End Sub
+
+    Public Sub Close()
+        Call New Thread(AddressOf __startCloseList).Start()
+    End Sub
+
+    Public Sub Open()
+        Call New Thread(AddressOf __startOpenList).Start()
+    End Sub
+End Class
