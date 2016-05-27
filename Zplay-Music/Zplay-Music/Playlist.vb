8de55df4e49beca0b065e16dfb7d01823f628fdc
xieguigang <xie.guigang@gmail.com>
2016-05-27 16:51:13 +0800

    file list model

--- Zplay-Music/Zplay-Music/Playlist.vb
+++ Zplay-Music/Zplay-Music/Playlist.vb
@@ -1,14 +1,40 @@
 ﻿Imports libZPlay.App
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 
 Public Class Playlist
 
-    Dim _files As Dictionary(Of String, MediaFile)
+    Dim _files As List(Of MediaFile)
+    Dim p As Integer = -1
+    Dim _eolist As Action
+
+    Sub New(EOList As Action)
+        _eolist = EOList
+    End Sub
 
     Sub New(files As IEnumerator(Of String))
-
+        _files = GetFilesInfo(files).ToList
     End Sub
 
     Public Function ReadNext() As String
+        p += 1
 
+        If p = _files.Count Then
+            Call _eolist()
+        Else
+            Return _files(p).FileName
+        End If
+
+        Return Nothing
+    End Function
+
+    Public Function ReadPrevious() As String
+        p -= 1
+        Return _files(p).FileName
+    End Function
+
+    Public Function Reset() As String
+        p = -1
+        Return ReadNext()
     End Function
 End Class
