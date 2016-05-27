a28092d711a113509317aac57bab1cb8e274385c
xieguigang <xie.guigang@gmail.com>
2016-05-28 01:10:05 +0800

    parser of cue head

--- libZPlay/App/Cue.vb
+++ libZPlay/App/Cue.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.ComponentModel.DataStructures
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Serialization
 
@@ -11,8 +12,36 @@ Public Class Cue : Inherits ClassObject
     Public Property Tracks As Track()
 
     Sub New(cue As String)
+        Dim lines As String() = cue.ReadAllLines
+        Dim i As New Pointer(Scan0)
+        Dim s As String = Nothing
+        Dim key As String
 
+        Do While lines(++i).ShadowCopy(s).FirstOrDefault <> " "c
+            If InStr(s, "REM", CompareMethod.Text) = 1 Then
+                s = Mid(s, 5)
+            End If
+            key = s.Split.First
+            s = Mid(s, key.Length + 1).Trim
+
+            If key.TextEquals("DATE") Then
+                [Date] = s.GetString
+            ElseIf key.TextEquals("GENRE") Then
+                Genre = s.GetString
+            ElseIf key.TextEquals("TITLE") Then
+                Title = s.GetString
+            ElseIf key.TextEquals("FILE") Then
+                Dim tokens As String() = CommandLine.GetTokens(s)
+                File = New NamedValue(Of String)(
+                    tokens(Scan0),
+                    tokens(1))
+            End If
+        Loop
+
+        Tracks = Track.TracksParser(lines.Skip(i).ToArray)
     End Sub
+
+
 End Class
 
 Public Class Track
@@ -26,5 +55,9 @@ Public Class Track
 
     Public Overrides Function ToString() As String
         Return Me.GetJson
+    End Function
+
+    Public Shared Function TracksParser(tokens As String()) As Track()
+
     End Function
 End Class
--- libZPlay/Test/Form1.vb
+++ libZPlay/Test/Form1.vb
@@ -1,41 +1,41 @@
-﻿Imports libZPlay.App
-Imports libZPlay.InternalTypes
-Imports Microsoft.VisualBasic.Serialization
+﻿'Imports libZPlay.App
+'Imports libZPlay.InternalTypes
+'Imports Microsoft.VisualBasic.Serialization
 
-Public Class Form1
-    Public Sub New()
+'Public Class Form1
+'    Public Sub New()
 
-        ' This call is required by the designer.
-        InitializeComponent()
+'        ' This call is required by the designer.
+'        InitializeComponent()
 
-        ' Add any initialization after the InitializeComponent() call.
+'        ' Add any initialization after the InitializeComponent() call.
 
 
-        Dim play As New libZPlay.App.MediaPlayer
+'        Dim play As New libZPlay.App.MediaPlayer
 
-        Call play.Playback("E:\116. 宇多田光Beautiful World.flac")
+'        Call play.Playback("E:\116. 宇多田光Beautiful World.flac")
 
-        Me.BackgroundImage = play.AlbumArt
+'        Me.BackgroundImage = play.AlbumArt
 
-        ticks = play.Playback()
-        '  play.SeeksByPercent(0.96)
-    End Sub
+'        ticks = play.Playback()
+'        '  play.SeeksByPercent(0.96)
+'    End Sub
 
-    Private Sub ticks_Tick(sender As MediaPlayer, cur As TStreamTime, p As Double) Handles ticks.Tick
-        Try
-            Call Me.Invoke(Sub() Text = $"[{Math.Round(p * 100, 2)}%] " & cur.GetJson)
-        Catch ex As Exception
+'    Private Sub ticks_Tick(sender As MediaPlayer, cur As TStreamTime, p As Double) Handles ticks.Tick
+'        Try
+'            Call Me.Invoke(Sub() Text = $"[{Math.Round(p * 100, 2)}%] " & cur.GetJson)
+'        Catch ex As Exception
 
-        End Try
-    End Sub
+'        End Try
+'    End Sub
 
-    Private Sub ticks_StateValidate(sender As MediaPlayer, stat As TStreamStatus) Handles ticks.StateValidate
-        MsgBox(stat.GetJson)
-    End Sub
+'    Private Sub ticks_StateValidate(sender As MediaPlayer, stat As TStreamStatus) Handles ticks.StateValidate
+'        MsgBox(stat.GetJson)
+'    End Sub
 
-    Private Sub ticks_EndOfTrack(sender As MediaPlayer) Handles ticks.EndOfTrack
-        MsgBox(sender.status.GetJson)
-    End Sub
+'    Private Sub ticks_EndOfTrack(sender As MediaPlayer) Handles ticks.EndOfTrack
+'        MsgBox(sender.status.GetJson)
+'    End Sub
 
-    Dim WithEvents ticks As TickEvent
-End Class
+'    Dim WithEvents ticks As TickEvent
+'End Class
