9dd5b72725019144413d9f08308163866e4857b7
xieguigang <xie.guigang@gmail.com>
2016-05-27 14:34:39 +0800

    try add tooltip function

--- Zplay-Music/Zplay-Music/ProgressIndicator.Designer.vb
+++ Zplay-Music/Zplay-Music/ProgressIndicator.Designer.vb
@@ -28,8 +28,9 @@ Partial Class ProgressIndicator
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
         Me.Name = "ProgressIndicator"
-        Me.Size = New System.Drawing.Size(578, 146)
+        Me.Size = New System.Drawing.Size(541, 4)
         Me.ResumeLayout(False)
 
     End Sub
--- Zplay-Music/Zplay-Music/ProgressIndicator.vb
+++ Zplay-Music/Zplay-Music/ProgressIndicator.vb
@@ -1,3 +1,18 @@
-﻿Public Class ProgressIndicator
+﻿Imports libZPlay.App
+Imports libZPlay.InternalTypes
+
+Public Class ProgressIndicator
+
+    Public Property Time As TStreamTime
+        Get
+            Return _time
+        End Get
+        Set(value As TStreamTime)
+            _time = value
+            Me.Text = value.FormatTime
+        End Set
+    End Property
+
+    Dim _time As TStreamTime
 
 End Class
