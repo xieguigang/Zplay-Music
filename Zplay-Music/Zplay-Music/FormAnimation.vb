e39908a3cfe6b9f7f08262d55335876d50ef3548
xieguigang <xie.guigang@gmail.com>
2016-05-27 14:22:31 +0800

    form animation success

--- Zplay-Music/Zplay-Music/FormAnimation.vb
+++ Zplay-Music/Zplay-Music/FormAnimation.vb
@@ -11,18 +11,55 @@ Public Class FormAnimation
     End Sub
 
     Public ReadOnly Property IsOpen As Boolean
+        Get
+            Return Parent.Location.X <> x
+        End Get
+    End Property
+
+    Dim x As Integer = -(563 - 22)
 
     Private Sub __waitListClose()
         Do While ListInvoke.IsOpen
             Call Thread.Sleep(10)
+            Call Application.DoEvents()
         Loop
     End Sub
 
+    Dim delta As Integer = 20
+
     Public Sub Close()
         Call __waitListClose()
+
+        Do While True
+
+            If Parent.Location.X > x Then
+                Parent.Location = New Point(Parent.Location.X - delta, Parent.Location.Y)
+            Else
+                Parent.Location = New Point(x, Parent.Location.Y)
+                Exit Do
+            End If
+
+            Call Application.DoEvents()
+            Call Thread.Sleep(2)
+        Loop
+
+        Parent.picDocker.BackgroundImage = My.Resources.Docker_Shrinkage
     End Sub
 
     Public Sub Open()
+        Do While True
 
+            If Parent.Location.X < 0 Then
+                Parent.Location = New Point(Parent.Location.X + delta, Parent.Location.Y)
+            Else
+                Parent.Location = New Point(0, Parent.Location.Y)
+                Exit Do
+            End If
+
+            Call Application.DoEvents()
+            Call Thread.Sleep(2)
+        Loop
+
+        Parent.picDocker.BackgroundImage = My.Resources.Docker_Expand
     End Sub
 End Class
