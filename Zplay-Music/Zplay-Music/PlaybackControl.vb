625e3ebd95f364d41ad9305bfbf326dd0b52eafa
xieguigang <xie.guigang@gmail.com>
2016-05-28 15:00:50 +0800

    playback controls

--- Zplay-Music/Zplay-Music/PlaybackControl.vb
+++ Zplay-Music/Zplay-Music/PlaybackControl.vb
@@ -1,5 +1,27 @@
 ﻿Public Class PlaybackControl
 
+    Public Event PlaybackNext()
+    Public Event PlaybackPrevious()
+    Public Event PlaybackPause()
+    Public Event PlaybackPlay()
+
+    Dim _paused As Boolean
+
+    Public Property Paused As Boolean
+        Get
+            Return _paused
+        End Get
+        Set(value As Boolean)
+            _paused = value
+
+            If _paused Then
+
+            Else
+
+            End If
+        End Set
+    End Property
+
     Private Sub btnNext_MouseEnter(sender As Object, e As EventArgs) Handles btnNext.MouseEnter
         btnNext.BackgroundImage = My.Resources.Next_highlight
     End Sub
@@ -9,7 +31,7 @@
     End Sub
 
     Private Sub btnNext_MouseUp(sender As Object, e As MouseEventArgs) Handles btnNext.MouseUp
-
+        RaiseEvent PlaybackNext()
     End Sub
 
     Private Sub btnPlay_MouseEnter(sender As Object, e As EventArgs) Handles btnPlay.MouseEnter
@@ -21,7 +43,11 @@
     End Sub
 
     Private Sub btnPlay_MouseUp(sender As Object, e As MouseEventArgs) Handles btnPlay.MouseUp
-
+        If Paused Then
+            RaiseEvent PlaybackPlay()
+        Else
+            RaiseEvent PlaybackPause()
+        End If
     End Sub
 
     Private Sub btnPrevious_MouseEnter(sender As Object, e As EventArgs) Handles btnPrevious.MouseEnter
@@ -33,6 +59,6 @@
     End Sub
 
     Private Sub btnPrevious_MouseUp(sender As Object, e As MouseEventArgs) Handles btnPrevious.MouseUp
-
+        RaiseEvent PlaybackPrevious()
     End Sub
 End Class
